4736fa9d08774d111761ba16f9eb097ff1c29e79
mabyoungg <mabyoungg@gmail.com>
2023-10-30 19:17:12 +0900

    setOut 테스트 코드 작성

--- src/test/java/org/example/TestUtilTest.java
+++ src/test/java/org/example/TestUtilTest.java
@@ -3,6 +3,7 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.io.ByteArrayOutputStream;
 import java.util.Scanner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,6 +24,23 @@ public class TestUtilTest {
         assertThat(cmd).isEqualTo("등록");
         assertThat(content).isEqualTo("내용");
         assertThat(author).isEqualTo("작가");
+
+    }
+
+    @Test
+    @DisplayName("setOutToByteArray 테스트")
+    void test2() {
+        ByteArrayOutputStream byteArrayOutputStream = TestUtil.setOutToByteArray();
+
+        System.out.println("1 / 홍길동 / 나는 천사다.");
+
+        String out = byteArrayOutputStream.toString().trim();
+
+        assertThat(out).isEqualTo("1 / 홍길동 / 나는 천사다.");
+
+//        TestUtil.clearSetOutToByteArray(byteArrayOutputStream);
+//        System.out.println(out);
+//        System.out.println("화면 출력");
 
     }
 }
