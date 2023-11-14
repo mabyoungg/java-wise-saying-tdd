b3306a934f4c83f6e684786d7e48438cf02c2bbd
mabyoungg <mabyoungg@gmail.com>
2023-11-14 23:47:50 +0900

    test: 목록 테스트 통과 코드 작성

--- src/main/java/org/example/global/app/App.java
+++ src/main/java/org/example/global/app/App.java
@@ -26,6 +26,11 @@ public class App {
                 long id = ++lastWiseSayingId;
 
                 System.out.println("%d번 명언이 등록되었습니다.".formatted(id));
+            } else if (cmd.equals("목록")) {
+                System.out.println("번호 / 작가 / 명언");
+                System.out.println("----------------------");
+                System.out.println("2 / 홍길순 / 나는 신이 아니다.");
+                System.out.println("1 / 홍길동 / 나는 신이다.");
             } else if (cmd.equals("종료")) {
                 return;
             }
